92f503d786adf76adfba4da5952f2f707e8e6412
Mohib ullah <>
2026-01-15 20:57:07 +0500

    update

--- app.js
+++ app.js
@@ -7,3 +7,35 @@ function showAlert() {
 function buyPhone(model) {
   alert("Thanks for purchasing a phone from us: " + model);
 }
+// Q3
+function deleteRow(btn) {
+  var row = btn.parentNode.parentNode;
+  row.parentNode.removeChild(row);
+}
+
+// Q4
+function changeImage(img) {
+  img.src = "nature2.jpg";
+}
+
+function resetImage(img) {
+  img.src = "nature1.jpg";
+}
+
+// Q5
+var counterValue = 0;
+
+var counterDisplay = document.getElementById("counterDisplay");
+
+var increaseBtn = document.getElementById("increaseBtn");
+var decreaseBtn = document.getElementById("decreaseBtn");
+
+increaseBtn.addEventListener("click", function() {
+    counterValue++;
+    counterDisplay.innerText = counterValue;
+});
+
+decreaseBtn.addEventListener("click", function() {
+    counterValue--;
+    counterDisplay.innerText = counterValue;
+});
